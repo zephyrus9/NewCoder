05519699194109525f2079c8a46b003bd5f3047a
sheng <xsleecumt@outlook.com>
2018-04-13 11:57:23 +0800

    4-13

--- Huawei/e23 删除字符串中出现次数最少的字符.py
+++ Huawei/e23 删除字符串中出现次数最少的字符.py
@@ -2,13 +2,23 @@
 # Author: 
 # 实现删除字符串中出现次数最少的字符，若多个字符出现次数一样，则都删除。输出删除这些单词后的字符串，字符串中其它字符保持原来的顺序。
 
-import sys
 
-def del_str(l):
+def del_min(l):
+    tmp = []
+    for i in l:
+        tmp.append(l.count(i))
+    min_count = min(tmp)
     res = []
-    c = 0
-    for i in list(l):
-        c = l.count(i)
+    for i in range(len(l)):
+        if tmp[i] != min_count:
+            res.append(l[i])
+        else:
+            pass
+    return ''.join(res)
 
-l = input()
-print(del_str(l))
+while True:
+    try:
+        l = input()
+        print(del_min(l))
+    except:
+        break
