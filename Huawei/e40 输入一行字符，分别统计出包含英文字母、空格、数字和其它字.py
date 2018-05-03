c6638e8c5a120fd7c42a37da9566a3121686c774
sheng <xsleecumt@outlook.com>
2018-05-03 21:58:52 +0800

    5.3

---
+++ Huawei/e40 输入一行字符，分别统计出包含英文字母、空格、数字和其它字.py
@@ -0,0 +1,21 @@
+# -*-coding: utf-8 -*-
+# Author: 
+def num_count(s):
+    a = [0,0,0,0]
+    for i in s:
+        if i.isalpha():
+            a[0] += 1
+        elif i.isspace():
+            a[1] += 1
+        elif i.isdigit():
+            a[2] += 1
+        else:
+            a[3] += 1
+    for i in a:
+        print(i)
+
+while True:
+    try:
+        num_count(input())
+    except Exception as e:
+        break
